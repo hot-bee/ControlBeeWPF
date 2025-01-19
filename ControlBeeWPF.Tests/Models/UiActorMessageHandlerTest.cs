0a2c887e33c16bd94c8cda04e7cd8bb46e9e2cce
Leo Kim <leo@hotbee.co>
2025-01-19 10:06:22 +0900

    SC-232 Introduce UiActorMessageHandler

---
+++ ControlBeeWPF.Tests/Models/UiActorMessageHandlerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Windows;
+using ControlBee.Models;
+using ControlBee.Services;
+using ControlBee.Variables;
+using ControlBeeWPF.Models;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ControlBeeWPF.Tests.Models;
+
+[TestSubject(typeof(UiActorMessageHandler))]
+public class UiActorMessageHandlerTest
+{
+    [Fact]
+    public void CallbackIntoMainThreadTest()
+    {
+        var ui = new UiActor(
+            new ActorConfig(
+                "ui",
+                new EmptyAxisFactory(),
+                new EmptyVariableManager(),
+                new FrozenTimeManager()
+            )
+        );
+
+        var called = false;
+        var sync1 = new AutoResetEvent(false);
+        var mainThread = new Thread(() =>
+        {
+            var application = new Application();
+            var handler = new UiActorMessageHandler(application.Dispatcher);
+            ui.SetHandler(handler);
+            ui.MessageArrived += (sender, message) =>
+            {
+                Assert.Equal("_hello", message.Name);
+                called = true;
+                application.Shutdown();
+            };
+            sync1.Set();
+            application.Run();
+        });
+        mainThread.Start();
+
+        sync1.WaitOne();
+        ui.Send(new Message(Actor.Empty, "_hello"));
+        mainThread.Join();
+        Assert.True(called);
+    }
+}
---
+++ ControlBeeWPF/Models/UiActorMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Threading;
+using ControlBee.Interfaces;
+using ControlBee.Models;
+
+namespace ControlBeeWPF.Models;
+
+public class UiActorMessageHandler(Dispatcher dispatcher) : IUiActorMessageHandler
+{
+    private Action<Message>? _publishMessage;
+
+    public void ProcessMessage(Message message)
+    {
+        dispatcher.Invoke(
+            (Message innerMessage) =>
+            {
+                _publishMessage?.Invoke(innerMessage);
+            },
+            message
+        );
+    }
+
+    public void SetCallback(Action<Message> publishMessage)
+    {
+        _publishMessage = publishMessage;
+    }
+}
