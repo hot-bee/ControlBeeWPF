3e1795c1a65c37ca410c1e2d08b9d5c0326995d8
Leo Kim <leo@hotbee.co>
2025-01-19 10:24:20 +0900

    SC-233 Introduce ToggleImageButton

---
+++ ControlBeeWPF/Components/ImageButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ControlBeeWPF.Components;
+
+public class ImageButton : Button
+{
+    public ImageButton(BitmapImage image, string text)
+    {
+        var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+        stackPanel.Children.Add(
+            new Image
+            {
+                Source = image,
+                Width = 16,
+                Height = 16,
+            }
+        );
+        stackPanel.Children.Add(
+            new TextBlock
+            {
+                Text = text,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5, 0, 0, 0),
+            }
+        );
+        Content = stackPanel;
+    }
+}
---
+++ ControlBeeWPF/Components/ToggleImageButton.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ControlBeeWPF.Components;
+
+public class ToggleImageButton : Button, INotifyPropertyChanged
+{
+    private readonly StackPanel _offContent;
+    private readonly StackPanel _onContent;
+    private bool _isChecked;
+
+    public ToggleImageButton(BitmapImage offImage, BitmapImage onImage, string text)
+    {
+        _offContent = new StackPanel { Orientation = Orientation.Horizontal };
+        _offContent.Children.Add(
+            new Image
+            {
+                Source = offImage,
+                Width = 16,
+                Height = 16,
+            }
+        );
+        _offContent.Children.Add(
+            new TextBlock
+            {
+                Text = text,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5, 0, 0, 0),
+            }
+        );
+
+        _onContent = new StackPanel { Orientation = Orientation.Horizontal };
+        _onContent.Children.Add(
+            new Image
+            {
+                Source = onImage,
+                Width = 16,
+                Height = 16,
+            }
+        );
+        _onContent.Children.Add(
+            new TextBlock
+            {
+                Text = text,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5, 0, 0, 0),
+            }
+        );
+
+        Content = _offContent;
+    }
+
+    public bool IsChecked
+    {
+        get => _isChecked;
+        set
+        {
+            if (SetField(ref _isChecked, value))
+                Content = _isChecked ? _onContent : _offContent;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected override void OnClick()
+    {
+        base.OnClick();
+        IsChecked = !IsChecked;
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
