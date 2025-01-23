3c1b767dd34eb40a63a6d042be70a292c0430bde
Leo Kim <leo@hotbee.co>
2025-01-23 14:36:29 +0900

    SC-274 Auto deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,45 @@
-$param0 = $args[0]
-if (!$args[0]) { 
-    Write-Host "Version must be provided."
-    exit 1
-}
-Write-Host "Deploy Version: $param0"
+param (
+    [switch] $up_version,
+    [switch] $major,
+    [switch] $minor,
+    [switch] $build
+)
+$ErrorActionPreference = "Stop"
 
-dotnet build --configuration Release
-dotnet pack --configuration Release
-dotnet nuget push "ControlBeeWPF/bin/Release/ControlBeeWPF.$param0.nupkg" --source "github"
+$Verfile = "version"
+$fileContents = (Get-Content $Verfile | Select -Last 1)
+
+if($up_version)
+{
+    $version = [Version]$fileContents
+    if($major) {
+        $newVersionString = (New-Object -TypeName 'System.Version' -ArgumentList @(($version.Major+1), 0, 0)).ToString()
+    }
+    elseif($minor) {
+        $newVersionString = (New-Object -TypeName 'System.Version' -ArgumentList @($version.Major, ($version.Minor+1), 0)).ToString()
+    }
+    elseif($build) {
+        $newVersionString = (New-Object -TypeName 'System.Version' -ArgumentList @($version.Major, ($version.Minor), ($version.Build+1))).ToString()
+    }
+    else {
+        throw "Please specify which part of the version should be updated."
+    }
+    $fileContents = $newVersionString
+    Write-Output $fileContents > $Verfile
+}
+$versionString = $fileContents
+
+Write-Host "Deploy Version: $versionString"
+
+dotnet pack --configuration Release -p:PackageVersion="$versionString"
+
+git add $Verfile
+git commit -m "Bump up version to $versionString"
+
+$tagName = "v$versionString"
+git tag $tagName
+
+git push
+git push origin tag $tagName
+
+dotnet nuget push "ControlBeeWPF/bin/Release/ControlBeeWPF.$versionString.nupkg" --source "github"
