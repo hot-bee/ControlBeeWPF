cfdae78ebf3443ab5b7e03007d491efee74d5a7f
Leo Kim <leo@hotbee.co>
2025-07-04 18:06:52 +0900

    TranslationExtension for localization

---
+++ ControlBeeWPF/Extensions/TranslateExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Markup;
+using ControlBee.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ControlBeeWPF.Extensions;
+
+public class TranslateExtension(string key) : MarkupExtension
+{
+    public string Key { get; set; } = key;
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return LocalizationManager.Instance.Translate(Key);
+    }
+}
