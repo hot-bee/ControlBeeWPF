6d6a5c0e38a65f016aa479708252766d964d74c9
Leo Kim <leo@hotbee.co>
2025-08-25 14:55:15 +0900

    Introduce RecipeManagerView

---
+++ ControlBeeWPF/ViewModels/RecipeManagerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ControlBee.Interfaces;
+using ControlBee.Services;
+
+namespace ControlBeeWPF.ViewModels;
+
+public partial class RecipeManagerViewModel : ObservableObject
+{
+    private readonly IVariableManager _variableManager;
+
+    [ObservableProperty] private string _localName;
+    [ObservableProperty] private string[] _localNames;
+
+    public RecipeManagerViewModel(IVariableManager variableManager)
+    {
+        _variableManager = variableManager;
+        LocalName = variableManager.LocalName;
+        LocalNames = variableManager.LocalNames;
+        variableManager.PropertyChanged += VariableManagerOnPropertyChanged;
+    }
+
+    private void VariableManagerOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(IVariableManager.LocalName):
+                LocalName = _variableManager.LocalName;
+                break;
+            case nameof(IVariableManager.LocalNames):
+                LocalNames = _variableManager.LocalNames;
+                break;
+        }
+    }
+}
---
+++ ControlBeeWPF/Views/RecipeManagerView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ControlBee.Interfaces;
+using ControlBeeWPF.Components;
+using ControlBeeWPF.ViewModels;
+
+namespace ControlBeeWPF.Views;
+
+/// <summary>
+///     Interaction logic for RecipeManagerView.xaml
+/// </summary>
+public partial class RecipeManagerView : UserControl
+{
+    private readonly IVariableManager _variableManager;
+    private readonly RecipeManagerViewModel _viewModel;
+
+    public RecipeManagerView(RecipeManagerViewModel viewModel, IVariableManager variableManager)
+    {
+        DataContext = viewModel;
+        _viewModel = viewModel;
+        _variableManager = variableManager;
+        InitializeComponent();
+    }
+
+    private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var recipeName = RecipeListBox.SelectedItem as string;
+        if (string.IsNullOrEmpty(recipeName)) return;
+        _variableManager.Load(recipeName);
+    }
+
+    private void SaveAsButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var inputBox = new InputBox();
+        if (inputBox.ShowDialog() is not true)
+            return;
+        var recipeName = inputBox.ResponseText;
+        if (string.IsNullOrEmpty(recipeName)) return;
+        _variableManager.Save(recipeName);
+    }
+
+    private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var recipeName = RecipeListBox.SelectedItem as string;
+        if (string.IsNullOrEmpty(recipeName)) return;
+        if (
+            MessageBox.Show(
+                $"Are you sure to DELETE \"{recipeName}\"?",
+                "Delete",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Warning
+            ) == MessageBoxResult.Yes
+        )
+            _variableManager.Delete(recipeName);
+    }
+}
