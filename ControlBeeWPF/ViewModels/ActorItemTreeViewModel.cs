28f77c1a5fdc5d657bbd96c1146b54b1d9a42f75
soon <tnsnz@github.com>
2025-12-01 18:24:39 +0900

    Add a filtered tree to ActorItemTreeViewModel to avoid corrupting the original.

--- ControlBeeWPF/ViewModels/ActorItemTreeViewModel.cs
+++ ControlBeeWPF/ViewModels/ActorItemTreeViewModel.cs
@@ -2,13 +2,41 @@
 
 namespace ControlBeeWPF.ViewModels;
 
-public class ActorItemTreeViewModel : ObservableObject
+public partial class ActorItemTreeViewModel : ObservableObject
 {
     private ActorItemTree _actorItemTreeCollection = new(new ActorItemViewModel { Name = "root" });
+    private ActorItemTree _filteredTree = new(new ActorItemViewModel { Name = "root" });
+
+    private readonly ActorItemTreeFilter _filter = new();
+
+    [ObservableProperty]
+    private string _filterText = string.Empty;
+
+    public void UpdateFilter()
+    {
+        var newRoot = _filter.Filter(ActorItemTreeCollection.Root, FilterText);
+        if (newRoot != null)
+            FilteredTreeCollection = new ActorItemTree(newRoot.Data) { Root = newRoot };
+    }
+
+    partial void OnFilterTextChanged(string value)
+    {
+        UpdateFilter();
+    }
 
     public ActorItemTree ActorItemTreeCollection
     {
         get => _actorItemTreeCollection;
-        set => SetProperty(ref _actorItemTreeCollection, value);
+        set
+        {
+            SetProperty(ref _actorItemTreeCollection, value);
+            _filteredTree = _actorItemTreeCollection;
+        }
+    }
+    
+    public ActorItemTree FilteredTreeCollection
+    {
+        get => _filteredTree;
+        set => SetProperty(ref _filteredTree, value);
     }
 }
