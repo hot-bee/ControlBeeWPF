e69e9a1a9a84b880b3f7edf80b33702a24edf469
kangyunkim <rkddbs0511@naver.com>
2026-02-06 15:01:27 +0900

    Add VariableView

---
+++ ControlBeeWPF/ViewModels/VariableGridViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ControlBee.Interfaces;
+using ControlBeeWPF.Models;
+
+namespace ControlBeeWPF.ViewModels;
+
+public sealed partial class VariableGridViewModel(IActorRegistry actorRegistry)
+{
+    public ObservableCollection<Cell> Cells { get; } = [];
+
+    public void AddVariable(int row, int col, ActorItemKey actorItemKey, string? cellName)
+    {
+        var variableViewModel = new VariableViewModel(
+            actorRegistry,
+            actorItemKey.ActorName,
+            actorItemKey.ItemPath,
+            actorItemKey.SubItemPath
+        );
+        var cell = new Cell(row, col, variableViewModel, cellName ?? variableViewModel.Name);
+        Cells.Add(cell);
+
+        if (!string.IsNullOrWhiteSpace(cellName))
+            return;
+        cell.CellName = variableViewModel.Name;
+        variableViewModel.PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(VariableViewModel.Name))
+                cell.CellName = variableViewModel.Name;
+        };
+    }
+
+    public sealed partial class Cell(int row, int col, VariableViewModel viewModel, string cellName)
+        : ObservableObject
+    {
+        public int Row { get; } = row;
+        public int Col { get; } = col;
+        public VariableViewModel ViewModel { get; } = viewModel;
+
+        [ObservableProperty]
+        private string _cellName = cellName;
+    }
+}
