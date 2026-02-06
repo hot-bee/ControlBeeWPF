99339a109b8cbf263de7aaa80e8b9891338920c7
kangyunkim <rkddbs0511@naver.com>
2026-02-06 15:10:22 +0900

    Add IOView

---
+++ ControlBeeWPF/ViewModels/DigitalOutputGridViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using ControlBee.Interfaces;
+using ControlBeeWPF.Models;
+
+namespace ControlBeeWPF.ViewModels;
+
+public sealed class DigitalOutputGridViewModel(IActorRegistry actorRegistry)
+{
+    public sealed class Cell(int row, int col, DigitalOutputViewModel viewModel)
+    {
+        public int Row { get; } = row;
+        public int Col { get; } = col;
+        public DigitalOutputViewModel ViewModel { get; } = viewModel;
+    }
+
+    public ObservableCollection<Cell> Cells { get; } = [];
+
+    public void AddVariable(int row, int col, ActorItemKey key)
+    {
+        var cell = new Cell(
+            row,
+            col,
+            new DigitalOutputViewModel(actorRegistry, key.ActorName, key.ItemPath)
+        );
+        Cells.Add(cell);
+    }
+}
