089252ead2cea212d77cd4e4c67f4c468e01e785
KangYun <116543523+kangyunkim@users.noreply.github.com>
2026-02-10 09:18:10 +0900

    Pull up LotoView to CBWPF (#31)
    * Increase the DataGrid row height in UserManagementView
    
    * Pull up LotoView to CBWPF
    
    * Resize controls in UserManagementView

---
+++ ControlBeeWPF/Views/LotoModeSelectionDialogView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Brushes = System.Windows.Media.Brushes;
+using Color = System.Windows.Media.Color;
+
+namespace ControlBeeWPF.Views;
+
+/// <summary>
+///     Interaction logic for LotoModeSelectionDialogView.xaml
+/// </summary>
+public partial class LotoModeSelectionDialogView
+{
+    public enum LotoModeSelection
+    {
+        Repair,
+        OnWorking,
+        RestTime,
+    }
+
+    public LotoModeSelection SelectedLotoMode { get; private set; } = LotoModeSelection.OnWorking;
+
+    public LotoModeSelectionDialogView()
+    {
+        InitializeComponent();
+        UpdateButtonVisuals();
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender.Equals(RepairButton))
+            SelectedLotoMode = LotoModeSelection.Repair;
+        else if (sender.Equals(OnWorkingButton))
+            SelectedLotoMode = LotoModeSelection.OnWorking;
+        else if (sender.Equals(RestTimeButton))
+            SelectedLotoMode = LotoModeSelection.RestTime;
+        else if (sender.Equals(OkButton))
+        {
+            DialogResult = true;
+            return;
+        }
+        else if (sender.Equals(CancelButton))
+        {
+            DialogResult = false;
+            return;
+        }
+
+        UpdateButtonVisuals();
+    }
+
+    private void UpdateButtonVisuals()
+    {
+        RepairButton.Opacity = 0.7;
+        OnWorkingButton.Opacity = 0.7;
+        RestTimeButton.Opacity = 0.7;
+
+        RepairButton.BorderBrush = Brushes.Transparent;
+        OnWorkingButton.BorderBrush = Brushes.Transparent;
+        RestTimeButton.BorderBrush = Brushes.Transparent;
+
+        RepairButton.BorderThickness = new Thickness(0);
+        OnWorkingButton.BorderThickness = new Thickness(0);
+        RestTimeButton.BorderThickness = new Thickness(0);
+
+        var selectedButton = SelectedLotoMode switch
+        {
+            LotoModeSelection.Repair => RepairButton,
+            LotoModeSelection.OnWorking => OnWorkingButton,
+            LotoModeSelection.RestTime => RestTimeButton,
+            _ => OnWorkingButton,
+        };
+
+        selectedButton.Opacity = 1.0;
+        selectedButton.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 215, 0));
+        selectedButton.BorderThickness = new Thickness(4);
+    }
+}
---
+++ ControlBeeWPF/Views/OperationBlockOverlayView.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ControlBee.Interfaces;
+using ControlBeeWPF.Interfaces;
+using Dict = System.Collections.Generic.Dictionary<string, object?>;
+using Message = ControlBee.Models.Message;
+using MessageBox = System.Windows.MessageBox;
+
+namespace ControlBeeWPF.Views;
+
+/// <summary>
+///     Interaction logic for OperationBlockOverlayView.xaml
+/// </summary>
+public partial class OperationBlockOverlayView
+{
+    private readonly string _password;
+    private readonly IActor _syncer;
+    private readonly IActor _ui;
+    private readonly IViewFactory _viewFactory;
+
+    public OperationBlockOverlayView(
+        IActorRegistry actorRegistry,
+        IViewFactory viewFactory,
+        Dict arguments
+    )
+    {
+        _viewFactory = viewFactory;
+        _syncer = actorRegistry.Get("Syncer")!;
+        _ui = actorRegistry.Get("Ui")!;
+
+        InitializeComponent();
+
+        Owner = (Window)arguments["Owner"]!;
+        JobNameLabel.Text = arguments["Job"]!.ToString();
+        _password = (string)arguments["Password"]!;
+
+        Loaded += (_, __) => ApplyOverlayBounds();
+        Closed += (_, _) => Owner.Activate();
+    }
+
+    private void ApplyOverlayBounds()
+    {
+        if (Owner == null)
+            return;
+
+        WindowStartupLocation = WindowStartupLocation.Manual;
+        Left = Owner.Left;
+        Top = Owner.Top;
+        Width = Owner.ActualWidth;
+        Height = Owner.ActualHeight;
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender.Equals(CloseButton))
+        {
+            if (Password.Password == _password)
+            {
+                _syncer.Send(new Message(_ui, "UnblockOperation"));
+                Close();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Incorrect password. Please try again.",
+                    "Password Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                Password.Clear();
+            }
+        }
+    }
+
+    private void Password_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        var keyboardView = _viewFactory.Create<KeyboardView>()!;
+        if (keyboardView.ShowDialog() is true)
+            Password.Password = keyboardView.Value;
+    }
+}
