f5367b8ed4798f32ea2cf18b42ac953635613d76
KangYun <116543523+kangyunkim@users.noreply.github.com>
2026-09-04 10:51:46 +0900

    Show channel tabs options to InspectionContainerView (#55)
    * Show channel tabs options to InspectionContainerView
    
    * Remoeve unnecessary code

--- ControlBeeWPF/Views/InspectionContainerView.xaml.cs
+++ ControlBeeWPF/Views/InspectionContainerView.xaml.cs
@@ -36,8 +36,9 @@ public partial class InspectionContainerView : IRefreshable, INotifyPropertyChan
         Loaded += OnLoaded;
 
         _mode = (string)_options["Mode"]!;
+        var showChannelTabs = _options.GetValueOrDefault("ShowChannelTabs") as bool? ?? true;
         var channelCount = systemConfigurations.VisionChannelCount;
-        if (_mode == "VisionFrame" && 1 < channelCount)
+        if (_mode == "VisionFrame" && showChannelTabs && 1 < channelCount)
         {
             var allButton = new Button { Content = "All", Tag = true };
             allButton.Click += (sender, args) =>
