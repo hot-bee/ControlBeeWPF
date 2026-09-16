e9626836bcd3f34339189a57195dd334222112e3
kangyunkim <rkddbs0511@naver.com>
2026-09-16 14:06:30 +0900

    Extract a view model from the double acting actuator status bar

--- ControlBeeWPF/Views/DoubleActingActuatorStatusBarView.xaml.cs
+++ ControlBeeWPF/Views/DoubleActingActuatorStatusBarView.xaml.cs
@@ -1,68 +1,54 @@
-﻿using System.Windows.Controls;
+using System.ComponentModel;
 using System.Windows.Input;
-using System.Windows.Media;
 using ControlBee.Interfaces;
-using ControlBee.Models;
+using ControlBeeWPF.ViewModels;
 using Brushes = System.Windows.Media.Brushes;
-using UserControl = System.Windows.Controls.UserControl;
 
 namespace ControlBeeWPF.Views;
 
 // ReSharper disable once InconsistentNaming
-public partial class DoubleActingActuatorStatusBarView : UserControl, IDisposable
+public partial class DoubleActingActuatorStatusBarView : IDisposable
 {
-    private readonly IActor _actor;
-    private readonly string _itemPath;
-    private readonly IActor _uiActor;
-    private readonly ActorItemBinder _binder;
-    private bool? _value;
+    private readonly DoubleActingActuatorViewModel _viewModel;
 
     public DoubleActingActuatorStatusBarView(
         IActorRegistry actorRegistry,
         string actorName,
         string itemPath
     )
+        : this(new DoubleActingActuatorViewModel(actorRegistry, actorName, itemPath)) { }
+
+    public DoubleActingActuatorStatusBarView(DoubleActingActuatorViewModel viewModel)
     {
+        _viewModel = viewModel;
+        DataContext = viewModel;
         InitializeComponent();
-        _itemPath = itemPath;
-        _actor = actorRegistry.Get(actorName)!;
-        _uiActor = actorRegistry.Get("Ui")!;
-        _binder = new ActorItemBinder(actorRegistry, actorName, itemPath);
-        _binder.MetaDataChanged += BinderOnMetaDataChanged;
-        _binder.DataChanged += Binder_DataChanged;
+        viewModel.PropertyChanged += ViewModelOnPropertyChanged;
+        UpdateView();
     }
 
     public void Dispose()
     {
-        _binder.MetaDataChanged -= BinderOnMetaDataChanged;
-        _binder.DataChanged -= Binder_DataChanged;
-        _binder.Dispose();
+        _viewModel.PropertyChanged -= ViewModelOnPropertyChanged;
+        _viewModel.Dispose();
     }
 
-    private void BinderOnMetaDataChanged(object? sender, Dictionary<string, object?> e)
+    private void ViewModelOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
-        NameLabel.Content = e["Name"];
-        ToolTip = e["Desc"];
+        UpdateView();
     }
 
-    private void Binder_DataChanged(object? sender, Dictionary<string, object?> e)
+    private void UpdateView()
     {
-        _value = (bool)e["CommandOn"]!;
-        InputOffRect.Fill = e["OffDetect"] is true ? Brushes.OrangeRed : Brushes.WhiteSmoke;
-        InputOnRect.Fill = e["OnDetect"] is true ? Brushes.OrangeRed : Brushes.WhiteSmoke;
-        ValueRect.Fill = _value is true ? Brushes.LawnGreen : Brushes.WhiteSmoke;
+        NameLabel.Content = _viewModel.Name;
+        ToolTip = _viewModel.ToolTip;
+        InputOffRect.Fill = _viewModel.OffDetect ? Brushes.OrangeRed : Brushes.WhiteSmoke;
+        InputOnRect.Fill = _viewModel.OnDetect ? Brushes.OrangeRed : Brushes.WhiteSmoke;
+        ValueRect.Fill = _viewModel.CommandOn ? Brushes.LawnGreen : Brushes.WhiteSmoke;
     }
 
     private void ValueRect_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        var newValue = _value is null or false;
-        _actor.Send(
-            new ActorItemMessage(
-                _uiActor,
-                _itemPath,
-                "_itemDataWrite",
-                new Dictionary<string, object?> { ["On"] = newValue }
-            )
-        );
+        _viewModel.ToggleValue();
     }
 }
